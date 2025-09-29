b8b35270d91177096aa32aa004c4e5f0d3d7c240
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2025-09-29 14:56:34 -0300

    feat: finishing header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,19 @@
 import logo from "../../assets/logo.png"
+import restart from "../../assets/restart.svg"
 import styles from "./styles.module.css"
 
 export function Header(){
     return <div className={styles.container}>
-        <img src={logo} alt="Logo" />{" "}
+        <img src={logo} alt="Logo" />
+
+        <header>
+            <span>
+                <strong>5</strong> de 10 tentativas
+            </span>
+
+            <button type="button">
+                <img src={restart} alt="icone de reiniciar" />
+            </button>
+        </header>
     </div>
 }
