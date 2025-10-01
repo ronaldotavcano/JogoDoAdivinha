7486c341dc845b05c8031c5bc850bcb8617f05f4
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2025-10-01 08:06:52 -0300

    feat: implementing letter component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.css"
 
 import {Header} from "./components/Header"
 import {Tip} from "./components/Tip"
+import {Letter} from "./components/Letter"
 
 export default function App(){
 
@@ -15,6 +16,8 @@ export default function App(){
                 <Header current={5} max={10} onRestart={handleRestartGame}/>
                 
                 <Tip tip="Linguagem de programação dinâmica"/>
+
+                <Letter value="R"/>
             </main>
         </div>
     )
