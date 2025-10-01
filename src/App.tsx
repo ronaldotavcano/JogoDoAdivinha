5fdf3fcb9d4e41607a2e84281e8697bbc9b79bb1
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2025-10-01 07:54:57 -0300

    feat: implementing Tip component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import styles from "./app.module.css"
 
 import {Header} from "./components/Header"
+import {Tip} from "./components/Tip"
 
 export default function App(){
 
@@ -12,6 +13,8 @@ export default function App(){
         <div className={styles.container}>
             <main>
                 <Header current={5} max={10} onRestart={handleRestartGame}/>
+                
+                <Tip tip="Linguagem de programação dinâmica"/>
             </main>
         </div>
     )
---
+++ src/components/Tip/index.tsx
@@ -0,0 +1,17 @@
+import styles from "./styles.module.css"
+import tipIcon from "../../assets/tip.svg"
+
+type Props = {
+    tip: string
+}
+
+export function Tip({tip}: Props){
+    return <div className={styles.tip}>
+            <img src={tipIcon} alt="icone de editar" />
+
+                <div>
+                    <h3>Dicas</h3>
+                    <p>{tip}</p>
+                </div>
+           </div>
+}
