be325aaaed999812c115f6bf300187bae261d6ee
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2025-10-01 08:32:57 -0300

    feat: implementing input component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styles from "./app.module.css"
 import {Header} from "./components/Header"
 import {Tip} from "./components/Tip"
 import {Letter} from "./components/Letter"
+import { Input } from "./components/input"
 
 export default function App(){
 
@@ -17,7 +18,18 @@ export default function App(){
                 
                 <Tip tip="Linguagem de programação dinâmica"/>
 
-                <Letter value="R"/>
+                <div className={styles.word}>
+                <Letter value="r"/>
+                <Letter value="e"/>
+                <Letter value="a"/>
+                <Letter value="c"/>
+                <Letter value="t"/>
+                </div>
+
+                <h4>Palpite</h4>
+                <div>
+                    <Input autoFocus maxLength={1} placeholder="?"/>
+                </div>
             </main>
         </div>
     )
