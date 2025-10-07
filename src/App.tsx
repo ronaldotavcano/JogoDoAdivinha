22fe4962ba83942ae588a08fc8f06b05a129dbc8
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2025-10-07 15:39:04 -0300

    feat: Implementing features for the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,121 @@
 import styles from "./app.module.css"
+import { useEffect, useState } from "react"
 
-import {Header} from "./components/Header"
+import { WORDS } from "./utils/words"
+import type { Challenge } from "./utils/words"
+
 import {Tip} from "./components/Tip"
-import {Letter} from "./components/Letter"
 import { Input } from "./components/input"
 import { Button } from "./components/Button"
+import {Letter} from "./components/Letter"
+import {Header} from "./components/Header"
 import { LettersUsed } from "./components/LettersUsed"
+import type {LettersUsedProps} from "./components/LettersUsed"
+
+const ATTEMPTS_MARGIN = 5
 
 export default function App(){
+    const [score, setScore] = useState(0)
+    const [letter, setLetter] = useState("")
+    const [lettersUsed, setLetttersUsed] = useState<LettersUsedProps[]>([])
+    const [challenge, setChallenge ] = useState<Challenge | null>(null)   
 
     function handleRestartGame(){
-        alert("Reiniciar o jogo!")
+        const isConfirmed = window.confirm("Você tem certeza de que deseja reiniciar? ")
+    
+        if (isConfirmed){
+            startGame()
+        }
+    }
+
+    function startGame(){
+        const index = Math.floor(Math.random() * WORDS.length)
+        const randomWord = WORDS[index]
+        setChallenge(randomWord)
+
+        setScore(0)
+        setLetttersUsed([])
+        setLetter("")
+    }
+
+    function handleConfirm(){
+        if (!challenge) {
+            return
+        }
+
+        if (!letter.trim()){
+            return alert("Digite uma letra!")
+        }
+
+        const value = letter.toUpperCase()
+        
+
+        const exists = lettersUsed.find((used) => used.value.toUpperCase() === value)
+        if (exists){
+            setLetter("")
+            return alert("Você já digitou a letra " +value)
+        }
+
+        const hits = challenge.word.toUpperCase().split("").filter((char) => char ===value).length
+
+        const correct = hits > 0
+        const currentScore = score + hits
+
+        setLetttersUsed((prevState) => [...prevState, { value, correct}])
+        setScore(currentScore)
+        setLetter("")
+    }
+
+    function endGame(message: string){
+        alert(message)
+        startGame()
+    }
+
+    useEffect( () =>{
+        startGame()
+    }, [])
+
+    useEffect( () => {
+        if(!challenge){
+            return
+        }
+        setTimeout(() => {
+            if (score === challenge.word.length){
+                return endGame("Parabéns, você descobriu a palavra")
+            }
+            const ATTEMPTS_LIMIT = challenge.word.length + ATTEMPTS_MARGIN
+            if (lettersUsed.length === ATTEMPTS_LIMIT){
+                return endGame("Que pena, você usou todas as tentativas")
+            }
+        }, 200)
+    }, [score, lettersUsed.length])
+
+    if (!challenge){
+        return null
     }
 
     return(
         <div className={styles.container}>
             <main>
-                <Header current={5} max={10} onRestart={handleRestartGame}/>
+                <Header current={lettersUsed.length} max={challenge.word.length + ATTEMPTS_MARGIN} onRestart={handleRestartGame}/>
                 
-                <Tip tip="Linguagem de programação dinâmica"/>
+                <Tip tip={challenge.tip}/>
 
                 <div className={styles.word}>
-                <Letter value="r"/>
-                <Letter value="e"/>
-                <Letter value="a"/>
-                <Letter value="c"/>
-                <Letter value="t"/>
+                    {challenge.word.split("").map((letter, index) => {
+                        const letterUsed = lettersUsed.find((used) => used.value.toUpperCase() === letter.toUpperCase())
+
+                        return <Letter key={index} value={letterUsed?.value} color={letterUsed?.correct ? "correct" : "default"}/> 
+                    })}                   
                 </div>
 
                 <h4>Palpite</h4>
                 <div className={styles.guess}>
-                    <Input autoFocus maxLength={1} placeholder="?"/>
-                    <Button title="Confirmar" />
+                    <Input autoFocus maxLength={1} placeholder="?" value={letter} onChange={(e) => setLetter(e.target.value)}/>
+                    <Button title="Confirmar" onClick={handleConfirm}/>
                 </div>
 
-                <LettersUsed />
+                <LettersUsed data={lettersUsed}/>
             </main>
         </div>
     )
